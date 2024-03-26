5c7ae7451580d91ee6ac6f781fea38220c68a3ab
mcfuddy2 <calvinglennburgess@gmail.com>
2024-03-25 18:43:58 -0600

    made a spell scraper

--- main.js
+++ main.js
@@ -1,127 +1,162 @@
 const { google } = require("googleapis");
 const { scrapeSpell } = require(`./scrapper.js`);
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
 async function updateSpellDescriptions() {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: "credentials.json",
-        scopes: "https://www.googleapis.com/auth/spreadsheets",
-    });
-    const client = await auth.getClient();
+	const auth = new google.auth.GoogleAuth({
+		keyFile: "credentials.json",
+		scopes: "https://www.googleapis.com/auth/spreadsheets",
+	});
+	const client = await auth.getClient();
 
-    const googleSheet = google.sheets({ version: "v4", auth: client });
+	const googleSheet = google.sheets({ version: "v4", auth: client });
 
-    const spreadsheetId = "1me_peELYXltqZ4BrfVfmhNVed0yZCYtY8JzrNl3rQcI";
+	const spreadsheetId = "1me_peELYXltqZ4BrfVfmhNVed0yZCYtY8JzrNl3rQcI";
 
-    try {
-        // Read from sheet
-        const metaData = await googleSheet.spreadsheets.values.get({
-            spreadsheetId: spreadsheetId,
-            range: "Spells!A2:AA550",
-        });
+	try {
+		// Read from sheet
+		const metaData = await googleSheet.spreadsheets.values.get({
+			spreadsheetId: spreadsheetId,
+			range: `Spells!D2:AA550`,
+		});
+
+		const spells = metaData.data.values.slice(1).map((row) => ({
+			spellName: row[0],
+			description: row[1],
+			level: row[2],
+			range: row[6],
+			castingTime: row[7],
+			duration: row[9],
+			concentration: row[10],
+			components: row[11],
+			ritual: row[12],
+			school: row[13],
+			source: row[14],
+			artificer: row[15],
+			bard: row[16],
+			cleric: row[17],
+			druid: row[18],
+			paladin: row[19],
+			ranger: row[20],
+			sorcerer: row[21],
+			warlock: row[22],
+			wizard: row[23],
+		}));
+
+		// Filter out spells with empty descriptions
+		const spellsToUpdate = spells.filter((spell) => !spell.description);
+
+
+
+        // source book abbreviations
+        const sourceMap = {
+            "Astral Adventures Guide": "AAG",
+            "Guildmasters Guide to Ravnica": "GGR",
+            "Fizbands Treasury of dragons": "FTD",
+            "Strixhaven": "STX",
+            "Acquisition Incorporated": "AI",
+            "Explorer's Guide to Wildmount": "EGTW",
+            "Tashas Cauldren of Everything": "TCE",
+            "Player's Handbook": "PHB",
+            "Xanathar's Guide to Everything": "XGTE",
+            "Xanathar's Guide to Everything/Elemental Evil Player's Companion": "XGTE",
+            "Icewind Dale": "IWD",
+            "Lost Labratory of Kwalish": "LLOK",
+            "Planescape: Adventures in the multiverse": "Plane",
+            "Book of Many Things": "BOMT",
+            "Elemental Evil Player's Companion": "EE"
+        };
+
+		// Iterate over spells with empty descriptions and scrape them
+		for (const spell of spellsToUpdate) {
+			try {
+				const spellInfo = await scrapeSpell(spell.spellName);
+
+				 // Reformat the level
+                 let formattedLevel = spellInfo.level.toLowerCase();
+                 if (formattedLevel === "cantrip") {
+                     formattedLevel = 0;
+                 } else {
+                     // Extract numeric part from strings like "2nd-level" and convert to integer
+                     formattedLevel = parseInt(formattedLevel);
+                 }
+                 spellInfo.level = formattedLevel; // Update the level with the reformatted value
         
-        const spells = metaData.data.values.slice(1).map((row) => ({
-            spellName: row[3],
-            description: row[4],
-            level: row[5],
-            range: row[9],
-            castingTime: row[10],
-            duration: row[12],
-            concentration: row[13],
-            components: row[14],
-            ritual: row[15],
-            school: row[16],
-            source: row[17],
-            artificer: row[18],
-            bard: row[19],
-            cleric: row[20],
-            druid: row[21],
-            paladin: row[22],
-            ranger: row[23],
-            sorcerer: row[24],
-            warlock: row[25],
-            wizard: row[26],
-        }));
+                 spellInfo.ritual = spellInfo.ritual ? 'Yes' : 'No';
+                 spellInfo.concentration = spellInfo.concentration ? 'Yes' : 'No';
 
-        // Filter out spells with empty descriptions
-        const spellsToUpdate = spells.filter((spell) => !spell.description);
+                 if (spellInfo.source && sourceMap[spellInfo.source]) {
+                    spellInfo.source = sourceMap[spellInfo.source];
+                }
+        
 
-        // Iterate over spells with empty descriptions and scrape them
-        for (const spell of spellsToUpdate) {
-            try {
-                const spellInfo = await scrapeSpell(spell.spellName);
+				// Iterate over each class and set the corresponding property in spellInfo
+				const classes = ["Artificer", "Bard", "Cleric", "Druid", "Paladin", "Ranger", "Sorcerer", "Warlock", "Wizard"];
+				classes.forEach((className) => {
+					spellInfo[className.toLowerCase()] = spellInfo.spellLists.includes(className) ? "x" : " ";
+				});
 
-                // Iterate over each class and set the corresponding property in spellInfo
-                const classes = ["Artificer", "Bard", "Cleric", "Druid", "Paladin", "Ranger", "Sorcerer", "Warlock", "Wizard"];
-                classes.forEach((className) => {
-                    spellInfo[className.toLowerCase()] = spellInfo.spellLists.includes(className) ? "x" : " ";
-                });
+				const spellRowToUpdate = spells.find((s) => s.spellName === spell.spellName);
+				const rowIndex = metaData.data.values.findIndex((row) => row[0] === spell.spellName) + 2;
 
-                const spellRowToUpdate = spells.find((s) => s.spellName === spell.spellName);
-                const rowIndex = spells.indexOf(spellRowToUpdate); // Adjust for spreadsheet row index starting from 2
-                const updateValues = [
-                    '', // Placeholder for column A (skipped)
-                    '', // Placeholder for column B (skipped)
-                    '', // Placeholder for column C (skipped)
-                    spellInfo.spellName,  // Column D
-                    spellInfo.description,  // Column E
-                    spellInfo.level,  // Column F
-                    '', // Placeholder for column G (skipped)
-                    '', // Placeholder for column H (skipped)
-                    '', // Placeholder for column I (skipped)
-                    spellInfo.range,  // Column J
-                    spellInfo.castingTime,  // Column K
-                    '', // Placeholder for column L (skipped)
-                    spellInfo.duration,  // Column M
-                    spellInfo.concentration,  // Column N
-                    spellInfo.components,  // Column O
-                    spellInfo.ritual,  // Column P
-                    spellInfo.school,  // Column Q
-                    spellInfo.source,  // Column R
-                    spellInfo.artificer,  // Column S
-                    spellInfo.bard,  // Column T
-                    spellInfo.cleric,  // Column U
-                    spellInfo.druid,  // Column V
-                    spellInfo.paladin,  // Column W
-                    spellInfo.ranger,  // Column X
-                    spellInfo.sorcerer,  // Column Y
-                    spellInfo.warlock,  // Column Z
-                    spellInfo.wizard,  // Column AA
-                ];
-                
+				const updateValues = [
+					spellInfo.spellName, // Placeholder for column D (skipped)
+					spellInfo.description, // Column E
+					spellInfo.level, // Column F
+					"", // Placeholder for column G (skipped)
+					"", // Placeholder for column H (skipped)
+					"", // Placeholder for column I (skipped)
+					spellInfo.range, // Column J
+					spellInfo.castingTime, // Column K
+					"", // Placeholder for column L (skipped)
+					spellInfo.duration, // Column M
+					spellInfo.concentration, // Column N
+					spellInfo.components, // Column O
+					spellInfo.ritual, // Column P
+					spellInfo.school, // Column Q
+					spellInfo.source, // Column R
+					spellInfo.artificer, // Column S
+					spellInfo.bard, // Column T
+					spellInfo.cleric, // Column U
+					spellInfo.druid, // Column V
+					spellInfo.paladin, // Column W
+					spellInfo.ranger, // Column X
+					spellInfo.sorcerer, // Column Y
+					spellInfo.warlock, // Column Z
+					spellInfo.wizard, // Column AA
+				];
 
-                // Update the spreadsheet using the correct row index
-                await googleSheet.spreadsheets.values.update({
-                    spreadsheetId: spreadsheetId,
-                    range: `Spells!B${rowIndex}`, // Use the calculated rowIndex
-                    valueInputOption: "USER_ENTERED",
-                    requestBody: {
-                        values: [updateValues],
-                    },
-                });
-            } catch (error) {
-                console.error(`Error updating spell ${spell.spellName}:`, error);
-            }
-        }
+				// Update the spreadsheet using the correct row index
+				await googleSheet.spreadsheets.values.update({
+					spreadsheetId: spreadsheetId,
+					range: `Spells!D${rowIndex}:AA${rowIndex}`, // Use the calculated rowIndex
+					valueInputOption: "USER_ENTERED",
+					requestBody: {
+						values: [updateValues],
+					},
+				});
+			} catch (error) {
+				console.error(`Error updating spell ${spell.spellName}:`, error);
+			}
+		}
 
-        console.log("Spell descriptions updated successfully.");
-    } catch (error) {
-        console.error("Error reading spreadsheet data:", error);
-        throw error; // Re-throw the error to be caught by the caller
-    }
+		console.log("Spell descriptions updated successfully.");
+	} catch (error) {
+		console.error("Error reading spreadsheet data:", error);
+		throw error; // Re-throw the error to be caught by the caller
+	}
 }
 
 app.get("/", async (req, res) => {
-    try {
-        await updateSpellDescriptions();
-        res.send("Spell descriptions updated successfully.");
-    } catch (error) {
-        console.error("An error occurred while updating spell descriptions:", error);
-        res.status(500).send("Internal server error.");
-    }
+	try {
+		await updateSpellDescriptions();
+		res.send("Spell descriptions updated successfully.");
+	} catch (error) {
+		console.error("An error occurred while updating spell descriptions:", error);
+		res.status(500).send("Internal server error.");
+	}
 });
 
-
-updateSpellDescriptions()
+updateSpellDescriptions();
--- scrapper.js
+++ scrapper.js
@@ -1,65 +1,146 @@
-const { default: axios } = require('axios');
-const cheerio = require('cheerio');
-
+const { default: axios } = require("axios");
+const cheerio = require("cheerio");
 
 async function scrapeSpell(spellName) {
-    try {
-        console.log(`I am scraping for ${spellName}`);
+	try {
+		console.log(`I am scraping for ${spellName}`);
+
+		//construct the website URL
+		if (!spellName) {
+			console.error("spellName is null or undefined");
+			return null;
+		}
+
+		const currentSiteToScrape = "http://dnd5e.wikidot.com/spell:";
+        let formattedSpellName = spellName.toLowerCase().replace(/\s/g, "-");
+
+        // Remove apostrophes if present in the spellName
+        formattedSpellName = formattedSpellName.replace(/'/g, '');
+        formattedSpellName = formattedSpellName.replace(/\//g, ' ');
         
-        //construct the website URL
-        if (!spellName) {
-            console.error('spellName is null or undefined');
-            return null;
+
+        const website = currentSiteToScrape + formattedSpellName;
+
+		// fetch HTML content of the site
+		const response = await axios.get(website);
+		
+        
+        
+        if (response.status === 200){
+            const html = response.data;
+
+		const $ = cheerio.load(html);
+
+		// Extract source, school, and level
+        const source = $("p").eq(0).text().split(":")[1].trim();
+        let schoolAndLevel = $("p").eq(1).text().split(" ");
+        let school = "";
+        let level = "";
+        let ritual = false;
+
+        // Check if the last element contains the "(ritual)" tag
+        if (schoolAndLevel[schoolAndLevel.length - 1].includes("(ritual)")) {
+            ritual = true;
+            // Remove the "(ritual)" tag from the last element
+            schoolAndLevel[schoolAndLevel.length - 1] = schoolAndLevel[schoolAndLevel.length - 1].replace("(ritual)", "").trim();
         }
 
-        const currentSiteToScrape = "http://dnd5e.wikidot.com/spell:";
-        const website = currentSiteToScrape + spellName.toLowerCase().replace(/\s/g, '-');
+        if (schoolAndLevel.includes("cantrip")) {
+            // Cantrip follows the format: school cantrip
+            school = schoolAndLevel[0];
+            level = "cantrip";
+        } else {
+            // Other spells follow the format: levelOfSpell school
+            level = schoolAndLevel[0];
+            school = schoolAndLevel.slice(1).join(" ");
+        }
 
-        // fetch HTML content of the site
-        const response = await axios.get(website);
-        const html = response.data;
 
-        const $ = cheerio.load(html);
 
-        // Extract source, school, and level
-        const source = $('p').eq(0).text().split(':')[1].trim();
-        const schoolAndLevel = $('p').eq(1).text().split(' ');
-        const school = schoolAndLevel[0];
-        const level = schoolAndLevel.slice(1).join(' ');
 
-        // Extract other details
-        const castingTime = $('strong:contains("Casting Time")').next().text().trim();
-        const range = $('strong:contains("Range")').next().text().trim();
-        const components = $('strong:contains("Components")').next().text().trim();
-        const duration = $('strong:contains("Duration")').next().text().trim();
-        const description = $('strong:contains("Description")').next().text().trim();
+		// Extract casting time using regular expression
+		const castingTimeRegex = /<strong>Casting Time:<\/strong>\s*([^<]+)/;
+		const castingTimeMatch = html.match(castingTimeRegex);
+		const castingTime = castingTimeMatch ? castingTimeMatch[1].trim() : "";
+
+		// Extract range using regular expression
+		const rangeRegex = /<strong>Range:<\/strong>\s*([^<]+)/;
+		const rangeMatch = html.match(rangeRegex);
+		const range = rangeMatch ? rangeMatch[1].trim() : "";
+
+		// Extract components using regular expression
+		const componentsRegex = /<strong>Components:<\/strong>\s*([^<]+)/;
+		const componentsMatch = html.match(componentsRegex);
+		const components = componentsMatch ? componentsMatch[1].trim() : "";
+
+		// Regular expression to match the duration, description, and "At Higher Levels" section
+		const descriptionRegex =
+			/<strong>Duration:<\/strong>\s*([^<]+)\s*<\/p>\s*<p>(.*?)<\/p>(?:\s*<p><strong><em>At Higher Levels.<\/em><\/strong>\s*(.*?)<\/p>)?/;
+
+		// Extract duration, description, and "At Higher Levels" section using the regular expression
+		const descriptionMatch = html.match(descriptionRegex);
+		let duration = "";
+		let description = "";
+		let atHigherLevels = "";
+
+		if (descriptionMatch) {
+			duration = descriptionMatch[1].trim();
+			description = descriptionMatch[2].trim();
+			atHigherLevels = descriptionMatch[3] ? descriptionMatch[3].trim() : "";
+
+			// Append the "At Higher Levels" section to the description if it exists
+			if (atHigherLevels) {
+				description += " At Higher Levels: " + atHigherLevels;
+			}
+		}
+
+		// Check if the duration contains "Concentration"
+		const concentration = duration.toLowerCase().includes("concentration");
+		if (concentration) {
+			// Remove "Concentration" from the duration
+			duration = duration.replace(/concentration,/i, "").trim();
+		}
+
+
+
 
         // Extract spell lists
         const spellLists = [];
-        $('strong:contains("Spell Lists")').next().find('a').each((index, element) => {
-            spellLists.push($(element).text());
+        $('strong:contains("Spell Lists")').nextAll('a').each((index, element) => {
+            spellLists.push($(element).text().trim());
         });
+        // Log the HTML of the element containing spell lists
+        console.log($('strong:contains("Spell Lists")').next('p').html());
 
-        // Create and return the spell object
-        const spell = {
-            source,
-            school,
-            level,
-            castingTime,
-            range,
-            components,
-            duration,
-            description,
-            spellLists
-        };
 
-        return spell;
-    } catch (error) {
-        console.error('Error scraping spell:', error);
+		// Create and return the spell object
+		const spell = {
+			spellName,
+			source,
+			school,
+			level,
+			castingTime,
+			range,
+			components,
+			duration,
+            ritual,
+			concentration,
+			description,
+			spellLists,
+		};
+
+		return spell;
+    } else if (response.status === 404){
+        console.log(`Spell page does not exist for ${spellName}`);
+        return null;
+    } else {
+        console.log(`non 404 error`)
         return null;
     }
+	} catch (error) {
+		console.error("Error scraping spell:", error);
+		return null;
+	}
 }
 
-
 module.exports = { scrapeSpell };
-
